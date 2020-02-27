543658173ff9f9713e260b066010e210cf2d8e3d
eGangotri <egangotritrust@gmail.com>
2020-02-27 22:10:17 +0800

    optimizations

--- src/com/egangotri/upload/archive/LoginToArchive.groovy
+++ src/com/egangotri/upload/archive/LoginToArchive.groovy
@@ -10,32 +10,16 @@ class LoginToArchive {
 
 
     static main(args) {
-
-/*        String acct = "indicjournals@gmail.com"
-        ARCHIVE_PROFILES.add("$acct")
-        ARCHIVE_PROFILES.add("jammumanuscripts@gmail.com")
-        ARCHIVE_PROFILES.add("swamianantabodha@gmail.com")
-        ARCHIVE_PROFILES.add("indologicalbooks@gmail.com")
-        ARCHIVE_PROFILES.add("srinagarbooks@gmail.com")
-        ARCHIVE_PROFILES.add("vintageindic@gmail.com")
-
-        (2..25).each { digit ->
-            ARCHIVE_PROFILES.add("$acct${digit.toString()}")
+        List archiveProfiles = EGangotriUtil.ARCHIVE_PROFILES
+        if (args) {
+            println "args $args"
+            archiveProfiles = args.toList()
         }
-
-        (27..30).each { digit ->
-            ARCHIVE_PROFILES.add("$acct${digit.toString()}")
-        }*/
-        ARCHIVE_PROFILES.addAll("DT", "JG", "IB", "RK", "NK", "UR", "SR", "MS", "NR", "ANON", "KECSS", "UPSS", "VRVDW", "SBLK", "SBLK2", "KALLA", "KRI",
-                "LCKN_PRE57", "LCKN_POST57", "OJHA", "VGBV", "TMKN", "RAMS", "UPSS_MANU", "RSS_MANU", "DT2", "EGANGOTRI_MANU", "EGANGOTRI_EBOOKS",
-                "RETRO_UPLOAD", "SCL_HYD", "ISHWAR", "MAHA_MUNI", "DATTA", "VIJAY", "VISIONARY_TUNES", "AJG")
-
-        ARCHIVE_PROFILES.each { println("$it")}
         log.info "login to Archive"
         def metaDataMap = UploadUtils.loadProperties(EGangotriUtil.ARCHIVE_PROPERTIES_FILE)
-        ARCHIVE_PROFILES*.toString().each { String archiveProfile ->
+        archiveProfiles*.toString().each { String archiveProfile ->
             println "Logging for Profile $archiveProfile"
-            ArchiveHandler.loginToArchive(metaDataMap, ArchiveHandler.ARCHIVE_LOGIN_URL, archiveProfile)
+            ArchiveHandler.loginToArchive(metaDataMap, archiveProfile)
         }
         println "***Browser Launches Done"
     }
