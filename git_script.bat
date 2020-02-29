0b4635f6dd7c3ce2bc529cac06bdd861407d0f62
eGangotri <egangotritrust@gmail.com>
2020-02-29 11:08:20 +0800

    optimizations Sat29-02-2020_11-08-19.68

--- git_script.bat
+++ git_script.bat
@@ -1,6 +1,9 @@
+set timestamp=%DATE:/=-%_%TIME::=-%
+set timestamp=%timestamp: =%
+
 git add src/*
 git add latestJarForUse/*
 git add test/*
-git commit -m "optimizations"
+git commit -m "optimizations %timestamp%"
 git push origin master
 git status
