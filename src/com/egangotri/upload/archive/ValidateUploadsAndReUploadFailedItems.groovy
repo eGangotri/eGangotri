0a87e7aff3ff8e32561101b5ee11d9c1b54ce007
eGangotri <egangotritrust@gmail.com>
2020-03-17 20:12:34 +0800

    Optimizations @ Tue03-17-2020_20-12-33.15

--- src/com/egangotri/upload/archive/ValidateUploadsAndReUploadFailedItems.groovy
+++ src/com/egangotri/upload/archive/ValidateUploadsAndReUploadFailedItems.groovy
@@ -36,9 +36,9 @@ class ValidateUploadsAndReUploadFailedItems {
         processUsheredCSV()
         processQueuedCSV()
         findQueueItemsNotInUsheredCSV()
-        filterFailedUsheredItems()
+      //filterFailedUsheredItems()
         //for use in special cases only
-        //generateFailedLinksFromStaticList()
+        generateFailedLinksFromStaticList()
         combineAllFailedItems()
         startReuploadOfFailedItems()
         System.exit(0)
@@ -96,24 +96,6 @@ class ValidateUploadsAndReUploadFailedItems {
         log.info("Converted " + identifierLinksForTesting.size() + " links of upload-ushered Item(s) from CSV in " + "Profiles ${archiveProfiles.toString()}")
     }
 
-    //This static variable can only be used with generateFailedLinksFromStaticList()
-    static  List<String> _staticListOfBadLinks = ["https://archive.org/details/kalidas_meghaduta_202003_406_"]
-
-    /** This method is used in unique cases. where u have failed indentifiers only and you want to use them to reupload them only
-     * // So u take the identifiers , use the ushered-csv to generate the complete VO Object.
-     * Then upload the VOS
-     */
-    static void generateFailedLinksFromStaticList(){
-        log.info("generating vos from static list of Links with size: " + _staticListOfBadLinks.size())
-
-        identifierLinksForTesting.eachWithIndex{ LinksVO entry, int i ->
-            if(_staticListOfBadLinks*.trim().contains(entry.archiveLink)){
-                println("entry.uploadLink: " + entry.uploadLink)
-                failedLinks << entry
-            }
-        }
-    }
-
     static void processQueuedCSV() {
         queuedItemsForTesting = ValidateLinksUtil.csvToItemsVO(queuedFile)
         Set queuedProfiles = queuedItemsForTesting*.archiveProfile as Set
@@ -160,7 +142,7 @@ class ValidateUploadsAndReUploadFailedItems {
             catch (Exception e) {
                 log.error("This is an Unsual Error. ${entry.archiveLink} Check Manually" + e.message)
                 e.printStackTrace()
-                failedLinks << entry
+                failedLinks << "'${entry}'"
             }
             if(i%75 == 0){
                 Thread.sleep(5000)
@@ -172,6 +154,34 @@ class ValidateUploadsAndReUploadFailedItems {
         log.info("Failed Links: " + failedLinks*.archiveLink.toString())
     }
 
+
+    //This static variable can only be used with generateFailedLinksFromStaticList()
+    static  List<String> _staticListOfBadLinks = ["https://archive.org/details/paribhashaindushekaranageshbhattachandrikabalbodhinied.sripadasatynaraynamurthir_374_T","https://archive.org/details/paribhashaindushekaranageshbhattavakyarthachandrikaharisastribhagavated.kallurkamashastri_741_C","https://archive.org/details/phitsutraofshantanavaacharyafranzkeilhorn1866_493_Z","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol1sampoornanaduniversity1977_749_S","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol1sampoornanaduniversity2000_685_p","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol2sampoornanaduniversity1977_359_f","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol2sampoornanaduniversity2000_876_X","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol3sampoornanaduniversity1955_662_O","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol3sampoornanaduniversity1980_965_w","https://archive.org/details/prakriyakaumudiramachandraprakasaed.srikrishnamuralidharmisravol3sampoornanaduniversity2000_450_d","https://archive.org/details/prakriyakaumudiramachandraprasadavitthalakamalashankarpranshankartrivedipart1bss_833_F","https://archive.org/details/prakriyakaumudiramachandraprasadavitthalakamalashankarpranshankartrivedipart2bss_915_M","https://archive.org/details/prakriyakaumudiramachandraprasadaofvitthalakamalashankarpranshankartrivedipart1bssalt_970_m","https://archive.org/details/prakriyakaumudiramachandraprasadaofvitthalakamalashankarpranshankartrivedipart2bssalt_57_z","https://archive.org/details/prakriyasarvasvanarayanabhattaed.kunhanrajac.universityofmadrasms_756_U","https://archive.org/details/prakriyasarvasvanaryanbhattaed.madhavanuniim.vol5universityoftravancore258_599_h","https://archive.org/details/praudamanoramabrihadbruhadsabdaratnaharidiksitalaghusabdaratnanagesabhattasitara_456_i","https://archive.org/details/praudamanoramakuchamadhinijagannathapanditprabhavibharatnadipaikasadashivasharmashastrichowk_525_w","https://archive.org/details/praudamanoramalaghusabdaratnaharidiksitabharavibhavaprakasasaralaed.bhairavamish_875_u","https://archive.org/details/praudamanoramalaghusabdaratnaharidiksitaed.ratnagopalabhattachowkambha_470_K","https://archive.org/details/praudamanoramasabdaratnaharidiksitabharavisabdaratnapradipikatikamadhavsastribha_150_V","https://archive.org/details/praudamanoramatathamanoramatikapothioroblong_177_q","https://archive.org/details/purvapakshavalied.unkownchowkambha_977_o","https://archive.org/details/readeronthesanskritgrammariansfritsstaalarticles_782_j","https://archive.org/details/sabdaapasabdavivekacharudevashastrinagpublications_852_c","https://archive.org/details/sabdaartharatnataranathatarkavachaspatibhattacharya1902_541_T","https://archive.org/details/sabdabodhamimamsaramanujatatacharyan.s.vol1frenchinstitute_444_g","https://archive.org/details/sabdabodhamimamsaramanujatatacharyan.s.vol2frenchinstitute_229_","https://archive.org/details/sabdabodhamimamsaramanujatatacharyan.s.vol3frenchinstitute_228_B","https://archive.org/details/sabdachandrikalakshimidharaed.kamalashankarpranshankartrivedibssprakrit_357_P","https://archive.org/details/sabdaindushekarsadashivabhattitikapothioroblong_389_","https://archive.org/details/sabdaindushekarchandrakaloodaraonsabdaindushekara_723_j","https://archive.org/details/sabdaindushekaravishamibhashyaraghavendracharyapothioroblonog_379_e","https://archive.org/details/sabdakaustubabhattojidikshitpothioroblong_633_J","https://archive.org/details/sabdakaustubabhattojidikshited.ganapatisastrimokatevindyesvariprasadadvivedivol2_129_n","https://archive.org/details/sabdakaustubabhattojidikshited.gopalshastrinenevol1chowkambha1adhyaya1pada_528_v","https://archive.org/details/sabdakaustubabhattojidikshited.gopalshastrinenevol2chowkambha2padaof1adhaya2pada3adhyaya_728_o","https://archive.org/details/sabdakaustubabhattojidikshited.ramakrishnashastrivol1chowkambha_466_M","https://archive.org/details/sabdakaustubabhattojidikshited.ramakrishnashastrivol2chowkambha_675_t","https://archive.org/details/sabdakaustubabhattojidikshited.ramakrishnashastrivol3chowkambha_92_F","https://archive.org/details/sabdakaustubabhattojidikshitvisamapadavyakhyanagesabhattajamesbenson_72_r","https://archive.org/details/sabdaratnataranathatarkavachaspatibhattacharyavyakarna_191_y","https://archive.org/details/sabdataranginisubrahmanyasastriv.sanskriteducationsocietymadras_332_d","https://archive.org/details/sadashivabhattivyakarna......................................................................._612_o","https://archive.org/details/sakatayanadhatupathacoverpagesmissing_624_","https://archive.org/details/sakatayanavyakaranachintamanivrittipanditedition_176_","https://archive.org/details/samskrtasanskritbhasavigyanaramadhinchaturvedichowkambha_495_","https://archive.org/details/sandhichandrikaramchandrajhachowkambha1952hindi_599_r","https://archive.org/details/sandhichandrikaramchandrajhachowkambha1977hindi_897_v","https://archive.org/details/sandhitheoreticalphoneticandhistoricalbasisofwordjuctioninsanskritsidneyallenw..","https://archive.org/details/sanskritgrammarmanuscriptsoffatherheinrichroths.j.16101668arnufcampsjeanclaudmullerbrill_940_O","https://archive.org/details/sanskritlanguageitsgrammarmadhavdeshpandem._952_K","https://archive.org/details/sanskritsyntaxgrammarofcasebrahmacharisurendrakumarksduniversitydarbangh_354_F","https://archive.org/details/sanskritvyakarandarsanramsureshtripathihindi_131_N","https://archive.org/details/sanskritvyakaranmenganapathkiparamparaauracharyapaninikapildevsastri1961_46_t","https://archive.org/details/sanskritvyakarnashastraitihasyudhistiramimamsakapart1_654_T","https://archive.org/details/sanskritvyakarnashastraitihasyudhistiramimamsakapart2_619_","https://archive.org/details/sanskritvyakarnashastraitihasyudhistiramimamsakapart3_151_E","https://archive.org/details/saramanjarijayakrishnatarkalankarawithcommentaryjivanandvidyasagara1935_51_J","https://archive.org/details/saraswatikanthibharanabhojachitraprakarnatikaratnerswarapandita_494_r","https://archive.org/details/saraswatikanthibharanabhojaanundoramborooah1884_728_","https://archive.org/details/saraswatikanthibharanabhojabhashyaramasimhajagaddharakedarnathsharmawasudevlaxma_934__","https://archive.org/details/saraswatikanthibharanabhojachintamanit.r.universityofmadrasms_449_f","https://archive.org/details/saraswatikanthibharanabhojaed.dravidaviresvarshastri4and5parichheda_613_F","https://archive.org/details/saraswatikanthibharanabhojaed.ramaswamisastriv.a.part4universityoftravancore154_673_K","https://archive.org/details/saraswatikanthibharanabhojaed.samabasivasastrik.part1universityoftravancore117_144_r","https://archive.org/details/saraswatikanthibharanabhojaed.samabasivasastrik.part2universityoftravancore127_100_","https://archive.org/details/saraswatikanthibharanabhojaed.samabasivasastrik.part3universityoftravancore140_31_v","https://archive.org/details/saraswatikanthibharanabhojaratnadarpanaratnesvaraed.kameshwarnathamishrapart1chowkambha_267_h","https://archive.org/details/saraswatikanthibharanabhojaratnadarpanaratnesvaraed.kameshwarnathamishrapart2chowkambha_313_C","https://archive.org/details/saraswatikanthibharanabhojaratnadarpanavyakhyaratnesvari_781_E","https://archive.org/details/saraswatikanthibharanabhojaratnesvarjagadharatikaed.visvanathbhattacharyabenarashinduuniversity_587_","https://archive.org/details/saraswatikanthibharanabhojaratnesvariratnesvaraandjivanandvidyasagar1894alt1_677_q","https://archive.org/details/saraswatikanthibharanabhojaratnesvariratnesvaraandjivanandvidyasagar1894alt_121_N","https://archive.org/details/saraswatikanthibharanabhojaratnesvariratnesvaraandjivanandvidyasagar1894_536_J","https://archive.org/details/saraswatikanthibharanabhojaonpoeticsed.sundarisiddharthavol1ignca_353_Z","https://archive.org/details/saraswatikanthibharanabhojaonpoeticsed.sundarisiddharthavol2ignca_704_K","https://archive.org/details/saraswatikanthibharanabhojaonpoeticsed.sundarisiddharthavol3ignca_242_T","https://archive.org/details/laghubhasyaonsaraswatavyakaranaofraghunathanagared.vamshidharavenkateswarasteampress1957_818_U","https://archive.org/details/sarasvatavyakarnaedshivadattashastri_177_n","https://archive.org/details/sarasvatavyakarnaguruprasadshastripart1_182_b","https://archive.org/details/sarasvatavyakarnatikaofkashirampathakvenkateswarasteampress_462_M","https://archive.org/details/sarasvatavyakarnawithvivarnaofmadhavatippaniofnanakramvenkateswarasteampress_293_G","https://archive.org/details/saraswatavyakaranabalabodhiindumatinaraharishastripendaseramachandrajhachowkambha_974_b","https://archive.org/details/saraswatavyakaranalaghubhashyavyakhyaragunathnagarvenkateswarasteampresssaraswati_323_b","https://archive.org/details/saraswatavyakaranasubodhikachandrakirtiprasadavausdevabhattinavakishorakarasharmavol1chowkambha_811_m","https://archive.org/details/saraswatavyakaranasubodhikachandrakirtiprasadavausdevabhattinavakishorakarasharmavol2chowkambha_828_","https://archive.org/details/saraswatavyakaranabyanaubhtisvarupacharyajivanandvidyasagarjivanandvidyasagar_832_","https://archive.org/details/saraswatavyakaranabyanaubhtisvarupacharyasubodhikaofchandrakirtinirnayasagarpress_829_U","https://archive.org/details/shabdaratnakarasadhusudharnaganied.haragovinddasbechardas_182_","https://archive.org/details/shridharishridharashastrilaghusabdaindusekaraed.dravidesvarashastri_83_Q","https://archive.org/details/siddhantachintamanivyakarnaprinciplesrulesindarsanaramprasadtripartisampoornanad","https://archive.org/details/4990010095964sikshadivedapandagan.a.446preligion.theologysanskrit0_14_O","https://archive.org/details/fouranusvarasoftaittiriyashakhashriramanasharma_291_q","https://archive.org/details/foursvarasoftaittiriyashakhashriramanasharma_680_Y","https://archive.org/details/katis39u-lipis39u-sam39skr39tam39-likhyate_612_x","https://archive.org/details/l39kaaratattvam39-nihitam39-guhaayaam_739_M","https://archive.org/details/raast39riya-shabda-saadhutvam_956_","https://archive.org/details/sam39skr39te-ardha-ekaarah39-okaarah39_500_t","https://archive.org/details/shiksaa-prathama-klptih_202003_436_o","https://archive.org/details/bharadvajasiksaramachandradikshitarv.r.sundaramayyarp.s.bori_960_V","https://archive.org/details/naradiyasiksasamavediyasatyavratasamasiromani1890_29_l","https://archive.org/details/naradiyasiksatikaofbhattashobhakaraed.datiyaswamipitambratrust_23_P","https://archive.org/details/naradiyasiksawithhinditika_567_o","https://archive.org/details/naradiyasiksawithcommentaryofbhattasobhakarapitambaraswami_746_R","https://archive.org/details/paninisiksamanmohangosh_980_","https://archive.org/details/paniniyasiksabacchulalavastibalakrishnasharmasantoshpandey......_137_","https://archive.org/details/paniniyasikshawithcommentarybechanramtripathi1877_234_s"]
+
+    /** This method is used in unique cases.
+     *  Where u have a list of failed Archive Urls and you want to use them to reupload them only
+     * So u take the links copy paste to _staticListOfBadLinks ,
+     * have following settings:
+     * IGNORE_QUEUED_ITEMS_IN_REUPLOAD_FAILED_ITEMS=true
+     * IGNORE_USHERED_ITEMS_IN_REUPLOAD_FAILED_ITEMS=false
+     * ONLY_GENERATE_STATS_IN_REUPLOAD_FAILED_ITEMS=false
+     *
+     * comment out call to filterFailedUsheredItems()
+     * uncomment call to generateFailedLinksFromStaticList() and execute tehe program
+     *
+     * .
+     * Then upload the VOS
+     */
+    static void generateFailedLinksFromStaticList(){
+        log.info("generating vos from static list of Links with size: " + _staticListOfBadLinks.size())
+
+        identifierLinksForTesting.eachWithIndex{ LinksVO entry, int i ->
+            if(_staticListOfBadLinks*.trim().contains(entry.archiveLink)){
+                println("entry.uploadLink: " + entry.uploadLink.replace("=eng", "=san"))
+                failedLinks << entry
+            }
+        }
+    }
     static void combineAllFailedItems(){
         if (missedOutQueuedItems || failedLinks) {
             allFailedItems = missedOutQueuedItems
