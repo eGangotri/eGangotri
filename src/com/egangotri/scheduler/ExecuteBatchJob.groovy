f6393d297c675b1fee9f09339df285495075bc8f
eGangotri <egangotritrust@gmail.com>
2020-04-04 19:27:59 +0800

    Optimizations @ Sat04-04-2020_19-27-58.98

---
+++ src/com/egangotri/scheduler/ExecuteBatchJob.groovy
@@ -0,0 +1,30 @@
+package com.egangotri.scheduler
+
+import com.egangotri.util.EGangotriUtil
+import org.quartz.Job
+import org.quartz.JobExecutionContext
+import org.quartz.JobExecutionException
+
+class ExecuteBatchJob implements Job {
+    static final String REMOTE_INSTRUCTIONS_FILE = EGangotriUtil.EGANGOTRI_BASE_DIR + File.separator + "google_drive" + File.separator + "cronJobInstructions.txt"
+    static final String DEFAULT_INSTRUCTION = "echo Hi @"
+    void execute(JobExecutionContext context)
+            throws JobExecutionException {
+
+        println("Cron Job Started");
+        //we have to clear it each time so that no instuction is repeated
+        File fileWithInstructions = new File(REMOTE_INSTRUCTIONS_FILE)
+        if(!fileWithInstructions.exists()){
+            fileWithInstructions.createNewFile()
+        }
+        String instructions = fileWithInstructions.getText('UTF-8')
+        if(!instructions){
+            instructions = DEFAULT_INSTRUCTION + new Date().format('YYYY-MM-DD HH:mm')
+        }
+        println "cmd /c ${instructions}".execute().text
+        fileWithInstructions.write(instructions)
+        //To reboot use
+        //"shutdown /r
+    }
+
+}
--- src/com/egangotri/scheduler/RemoteJobScheduler.groovy
+++ src/com/egangotri/scheduler/RemoteJobScheduler.groovy
@@ -8,9 +8,10 @@ import org.quartz.Trigger
 import org.quartz.TriggerBuilder
 import org.quartz.impl.StdSchedulerFactory
 
-class ExecuteScheduledJob {
-    static void main(String[] args) throws Exception {
-        JobDetail job = JobBuilder.newJob(HelloJob.class)
+class RemoteJobScheduler {
+    static int CRON_JOB_FREQUENCY_IN_MINUTES = 5
+    static void main(def args) throws Exception {
+        JobDetail job = JobBuilder.newJob(ExecuteBatchJob.class)
                 .withIdentity("executeRemotelyJob", "team-viewer-server-1").build();
         // Trigger the job to run on the next round minute
         Trigger trigger = TriggerBuilder
@@ -18,11 +19,11 @@ class ExecuteScheduledJob {
                 .withIdentity("executeRemotelyTrigger", "team-viewer-server-1")
                 .withSchedule(
                         SimpleScheduleBuilder.simpleSchedule()
-                                .withIntervalInMinutes(5).repeatForever())
+                                .withIntervalInMinutes(CRON_JOB_FREQUENCY_IN_MINUTES).repeatForever())
                 .build();
         // schedule it
-        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-        scheduler.start();
-        scheduler.scheduleJob(job, trigger);
+        Scheduler _scheduler = new StdSchedulerFactory().getScheduler();
+        _scheduler.start();
+        _scheduler.scheduleJob(job, trigger);
     }
 }
