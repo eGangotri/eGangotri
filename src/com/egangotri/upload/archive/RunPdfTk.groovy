1da67ec0340d95691a713d23d97d9196d7af0913
eGangotri <egangotritrust@gmail.com>
2020-04-10 00:55:19 +0800

    Optimizations @ Fri04-10-2020_0-55-18.99

--- src/com/egangotri/upload/archive/RunPdfTk.groovy
+++ src/com/egangotri/upload/archive/RunPdfTk.groovy
@@ -8,12 +8,16 @@ import java.nio.file.Files
 
 @Slf4j
 class RunPdfTk {
+    static File directory = new File(EGangotriUtil.CODE_404_BAD_DATA_FOLDER+File.separator+"pdf")
 
+    static File _repaired = new File(directory,"_repaired")
+    static File _altered = new File(directory, "_altered")
     static main(args) {
-        File directory = new File(EGangotriUtil.CODE_404_BAD_DATA_FOLDER+File.separator+"pdf")
-        File _repaired = new File(directory,"_repaired")
         if(!_repaired.exists()){
             _repaired.mkdir()
+        }
+        if(!_altered.exists()){
+            _altered.mkdir()
         }
         File[] files = directory.listFiles()
         files.eachWithIndex { File file, int _counter ->
@@ -24,13 +28,15 @@ class RunPdfTk {
     }
 
     static void repairPdf(File src, File destDir, String _counter = ""){
-        String repairedFileName = src.name.replace('.pdf','_rep.pdf')
-        String dest = destDir.absolutePath + File.separator + repairedFileName
-        String execInstruction = "cmd /c pdftk \"${src}\"  output \"${dest}\""
+        String repairedFile = destDir.absolutePath + File.separator + src.name.replace('.pdf','_rep.pdf')
+        String execInstruction = "cmd /c pdftk \"${src}\"  output \"${repairedFile}\""
         log.info("$_counter"+ execInstruction)
         try{
             execInstruction.execute().text
-            log.info("after execution of  ${src.name}")
+            if(new File(repairedFile).exists()){
+                log.info("after execution of  ${src.name}")
+                Files.copy(src.toPath(), new File(_altered, src.name).toPath())
+            }
         }
         catch(Exception ex){
             ex.printStackTrace()
