360e8a0d4cf2c4ef122f059417a57b30221b36ff
eGangotri Trust <egangotritrust@gmail.com>
2022-02-04 22:40:38 +0530

    Optimizations @ 04-02-2022_22-40-37.65

--- src/com/egangotri/pdf/EGangotriPDFMerger.groovy
+++ src/com/egangotri/pdf/EGangotriPDFMerger.groovy
@@ -18,58 +18,56 @@ class EGangotriPDFMerger {
 
     static void main(String[] args) {
         List<String> _mergeables = []
-        if(args){
-            if(args.length == 2 && args[1] == MEGA_TYPE){
+        if (args) {
+            if (args.length == 2 && args[1] == MEGA_TYPE) {
                 _mergeables = GenericUtil.getDirectories(args[0])*.absolutePath
                 GenericUtil.addReport("""MegaMerge for Folders:\n${_mergeables.join("\n")}  
                 started""")
-            }
-            else _mergeables = args;
+            } else _mergeables = args;
             int counter = 0
-            for(String _mergeable: _mergeables){
+            for (String _mergeable : _mergeables) {
                 counter++
                 GenericUtil.addReport("Merge for Folder(${_mergeable})\n ${counter} of ${_mergeables.size()} for ${_mergeable} started")
                 exec(_mergeable)
             }
-        }
-       else{
+        } else {
             exec(ROOT_FOLDER)
         }
         Tally.printMEGA_TALLY_OBJECT()
     }
 
-    static void exec(String _mergeable){
+    static void exec(String _mergeable) {
         try {
             File rootDir = new File(_mergeable)
             File[] foldersWithPdf = GenericUtil.getDirectories(rootDir)
             Date startTime = new Date()
             GenericUtil.addReport("""Merge started @ ${startTime} for 
 ${rootDir.name} for ${foldersWithPdf?.size()} Folder(s) :
-            ${foldersWithPdf.collect{it.name}.join("\n\t")}
+            ${foldersWithPdf.collect { it.name }.join("\n\t")}
             started""")
             int counter = 0
             for (File subFolder in foldersWithPdf) {
                 counter++
-                GenericUtil.addReport( "${counter} of ${foldersWithPdf.length}) Process folder \n ${subFolder.name}")
-                try{
+                GenericUtil.addReport("${counter} of ${foldersWithPdf.length}) Process folder \n ${subFolder.name}")
+                try {
                     mergeSmallerPdfs(subFolder)
                     GenericUtil.garbageCollectAndPrintMemUsageInfo()
                     mergeFinalPdf(subFolder)
                     GenericUtil.garbageCollectAndPrintMemUsageInfo()
                 }
 
-                catch(Exception e){
-                    log.info("Error in Process Merge",e)
+                catch (Exception e) {
+                    log.info("Error in Process Merge", e)
                     continue
                 }
                 GenericUtil.garbageCollectAndPrintMemUsageInfo()
             }
             Date endTime = new Date()
-            GenericUtil.addReport("Merge finishes ${endTime}. Time Taken: ${TimeUtil.formattedTimeDff(endTime,startTime)}")
+            GenericUtil.addReport("Merge finishes ${endTime}. Time Taken: ${TimeUtil.formattedTimeDff(endTime, startTime)}")
             tallyPostMerge(rootDir)
 
         } catch (Exception e) {
-            log.info("Exception in merge.outer ",e)
+            log.info("Exception in merge.outer ", e)
         }
         GenericUtil.printReport()
     }
@@ -90,29 +88,33 @@ ${rootDir.name} for ${foldersWithPdf?.size()} Folder(s) :
         }
     }
 
-    static void mergeFinalPdf(File subFolders){
+    static void mergeFinalPdf(File subFolders) {
         File[] pdfFiles = GenericUtil.getPdfsSortedByName(new File(subFolders, PDFS_MERGE_FOLDER))
         //log.info("pdfFiles ${pdfFiles.join("\n")}")
-        if(pdfFiles){
-            String finalPdfDumpFolder =  subFolders.getParentFile().getAbsolutePath() + "//${FINAL_PDFS}//"
-            if(!new File(finalPdfDumpFolder).exists()){
+        if (pdfFiles) {
+            String finalPdfDumpFolder = subFolders.getParentFile().getAbsolutePath() + "//${FINAL_PDFS}//"
+            if (!new File(finalPdfDumpFolder).exists()) {
                 new File(finalPdfDumpFolder).mkdir()
             }
             String finalPdf = finalPdfDumpFolder + subFolders.name + ".pdf"
-            GenericUtil.addReport( "Final Merge to ${GenericUtil.ellipsis(subFolders.name)}..${GenericUtil.reverseEllipsis(finalPdf)}")
+            GenericUtil.addReport("Final Merge to ${GenericUtil.ellipsis(subFolders.name)}..${GenericUtil.reverseEllipsis(finalPdf)}")
             PdfMergeCoreLogicIText5.doMerge(pdfFiles, finalPdf)
         }
     }
 
-    static void tallyPostMerge(File pdfFolder){
-        String tiffFolderPath = PdfUtil.extractTiffFolderName(pdfFolder)
-        String finalPdfsPath = "${pdfFolder}\\${FINAL_PDFS}"
-        if(tiffFolderPath){
-            log.info("Tally ${tiffFolderPath} against ${finalPdfsPath}")
-            Tally.main(tiffFolderPath, finalPdfsPath)
+    static void tallyPostMerge(File pdfFolder) {
+        try {
+            String tiffFolderPath = PdfUtil.extractTiffFolderName(pdfFolder)
+            String finalPdfsPath = "${pdfFolder}\\${FINAL_PDFS}"
+            if (tiffFolderPath) {
+                log.info("Tally ${tiffFolderPath} against ${finalPdfsPath}")
+                Tally.main(tiffFolderPath, finalPdfsPath)
+            } else {
+                log.info("cant Tally ${tiffFolderPath} against ${finalPdfsPath}")
+            }
         }
-        else{
-            log.info("cant Tally ${tiffFolderPath} against ${finalPdfsPath}")
+        catch (Exception e) {
+            log.info("Exception in post merge tally", e)
         }
     }
 }
--- src/com/egangotri/pdf/PdfUtil.groovy
+++ src/com/egangotri/pdf/PdfUtil.groovy
@@ -8,7 +8,14 @@ class PdfUtil {
     static String extractTiffFolderName(File pdfFolder){
         String[] _splitBy =  pdfFolder.name.split("_")
         String extractedDate = (_splitBy?.size()> 1 && _splitBy[1].size() > 10) ? _splitBy[1].substring(0,10) : ""
-        String tiffFolderPath = "${NMM_PATH}${pdfFolder.getParentFile().name}\\${extractedDate}"
+
+        String parentFileName = pdfFolder.getParentFile().name
+        if(parentFileName.contains("_(")){
+            String[] prefix = parentFileName.split("_\\(")
+            parentFileName = prefix[0]
+        }
+
+        String tiffFolderPath = "${NMM_PATH}${parentFileName}\\${extractedDate}"
         if(extractedDate && new File(tiffFolderPath).exists()){
             return tiffFolderPath;
         }
