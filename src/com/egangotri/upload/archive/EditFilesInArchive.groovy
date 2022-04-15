dc8222fe69c0835f1547d125c5e442c196374b99
egangotri <egangotritrust@gmail.com>
2022-04-15 15:57:03 +0800

    Optimizations @ Fri15-04-2022_15-57-02.61

---
+++ src/com/egangotri/upload/archive/EditFilesInArchive.groovy
@@ -0,0 +1,63 @@
+package com.egangotri.upload.archive
+
+import com.egangotri.upload.util.ArchiveUtil
+import com.egangotri.upload.util.UploadUtils
+import com.egangotri.util.EGangotriUtil
+import groovy.util.logging.Slf4j
+import org.openqa.selenium.By
+import org.openqa.selenium.chrome.ChromeDriver
+
+import static com.egangotri.upload.util.ArchiveUtil.getResultsCount
+
+@Slf4j
+class EditFilesInArchive {
+    static int MAX_ITEMS_RETRIEVABLE = 10
+    static void main(String[] args) {
+        List<String> archiveProfiles = EGangotriUtil.ARCHIVE_PROFILES
+        if (args) {
+            log.info "args $args"
+            archiveProfiles = args.toList()
+        }
+        EGangotriUtil.recordProgramStart("eGangotri Archive Logger")
+        def metaDataMap = UploadUtils.loadProperties(EGangotriUtil.ARCHIVE_PROPERTIES_FILE)
+        if(archiveProfiles.last().isInteger()){
+            MAX_ITEMS_RETRIEVABLE = archiveProfiles.last().toInteger()
+            archiveProfiles = archiveProfiles.take(archiveProfiles.size()-1);
+        }
+        log.info "archiveProfiles $archiveProfiles ${MAX_ITEMS_RETRIEVABLE}"
+
+        archiveProfiles.each { String archiveProfile ->
+            log.info "Logging for Profile $archiveProfile"
+            ChromeDriver driver = new ChromeDriver()
+            if(ArchiveUtil.navigateLoginLogic(driver, metaDataMap, archiveProfile)){
+                getResultsCount(driver, true)
+                UploadUtils.maximizeBrowser(driver)
+                def attributeList =
+                        driver.findElements(By.xpath("//div[@class='item-ia hov']"))
+                                *.getAttribute("data-id")
+                MAX_ITEMS_RETRIEVABLE = attributeList.size()>MAX_ITEMS_RETRIEVABLE
+                        ?MAX_ITEMS_RETRIEVABLE:attributeList.size();
+
+                log.info "MAX_ITEMS_RETRIEVABLE ${MAX_ITEMS_RETRIEVABLE}"
+                attributeList?.take(MAX_ITEMS_RETRIEVABLE)?.forEach( dataId -> {
+                    try {
+                        String editableUrl = "https://archive.org/editxml/${dataId}"
+                        log.info("editableUrl ${editableUrl}")
+                        UploadUtils.openNewTab(driver)
+                        UploadUtils.switchToLastOpenTab(driver)
+                        driver.navigate().to(editableUrl)
+                        driver.get(editableUrl)
+                    }
+                    catch(Exception e){
+                        log.info("exception handling dataId ${dataId}",e)
+                    }
+                })
+            }
+        }
+        EGangotriUtil.recordProgramEnd()
+        System.exit(0)
+    }
+
+
+}
+
