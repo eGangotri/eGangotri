6c4e3be11ea8322ca6eb57a81cd9e6f4b7c68ca8
egangotri <egangotritrust@gmail.com>
2022-10-24 15:21:07 +0800

    get/post now using HttpBuilder

--- src/com/egangotri/rest/RestUtil.groovy
+++ src/com/egangotri/rest/RestUtil.groovy
@@ -11,26 +11,40 @@ import static groovy.json.JsonOutput.toJson
 @Slf4j
 class RestUtil {
     static SimpleDateFormat dateFormat = new SimpleDateFormat(EGangotriUtil.DATE_TIME_PATTERN)
-    static postCaller(String uri = 'http://127.0.0.1:80',
-                      String path = '/itemsQueued/list',
-                      Map body = [title: 'food', body: 'bar', userId: 1]) {
+    static String REST_CALL_URI = 'http://127.0.0.1:80'
+
+    static postCaller(String path,
+                      Map body,
+                      String uri = REST_CALL_URI) {
         def posts
         try {
             posts = configure {
-                request.uri = 'https://jsonplaceholder.typicode.com'
-                request.uri.path = '/posts'
+                request.uri = uri
+                request.uri.path = path
                 request.contentType = 'application/json'
                 request.body = toJson(body)
             }.post()
             log.info("posts ${posts}")
         }
         catch(Exception e){
-            log.info("post call failed for ${uri}/${path}",e)
+            log.info("MakeRestCall Error while calling ${uri}/${path}", e)
         }
         return posts
     }
 
-    static String makeGetCall(String urlString = 'https://postman-echo.com/get') {
+    static String makeGetCall(String path, Map queryMap = [name: 'Bob'], String urlString = REST_CALL_URI)
+    {
+        def doGet = configure {
+            request.uri = urlString
+        }.get {
+            request.uri.path = path
+            request.uri.query = queryMap?:[:]
+        }
+        log.info("doGet ${doGet}")
+        return doGet
+    }
+
+    static String makeGetCall2(String urlString = 'https://postman-echo.com/get') {
         def res
         URL postmanGet = new URL(urlString)
         try {
@@ -79,8 +93,7 @@ class RestUtil {
         }
         return jsonSlurper.parseText(text)
     }
-
-    static String createPostParamsString(String ...params) {
+    static String createPostParamsStringDiscard(String ...params) {
         if(params.length !=6 ){
             log.info("exactly 6 params expected")
             return
@@ -103,14 +116,36 @@ class RestUtil {
         log.info("paramsDecorated ${paramsDecorated}")
         return paramsDecorated
     }
+    static Map createPostParamsAsMap(String ...params) {
+        if(params.length !=7 ){
+            log.info("exactly 7 params expected")
+            return [:]
+        }
+
+        int counter = 0
+        Map paramsMap = [:]
+        paramsMap.put ("archiveProfile","${params[counter++]}")
+        paramsMap.put("uploadLink", "${params[counter++]}")
+        paramsMap.put("localPath","${params[counter++]}")
+        paramsMap.put("title","${params[counter++]}")
+        paramsMap.put("uploadCycleId", "${params[counter++]}")
+        paramsMap.put("csvName", "${params[counter++]}")
+
+        paramsMap.put("datetimeUploadStarted", "${dateFormat.format(new Date())}")
+        log.info("paramsMap ${paramsMap}")
+        return paramsMap
+    }
+
     static void main(String[] args) {
         //makePostCall()
-        int counter = 1;
-        String paramsDecorated = createPostParamsString("${counter++}", "${counter++}", "${counter++}", "${counter++}", "${counter++}", "${counter++}")
-        postCaller()
+        int counter = 11233456;
+        Map paramsDecorated = createPostParamsAsMap("${counter++}", "${counter++}", "${counter++}", "${counter++}", "${counter++}", "${counter++}", "${counter++}")
+        //postCaller('/itemsQueued/add', paramsDecorated)
+        //postCaller('posts', [title: 'food', body: 'bar', userId: 1], 'https://jsonplaceholder.typicode.com')
 //        makePostCall("http://127.0.0.1:80/itemsQueued/add",
 //        paramsDecorated)
-//        makeGetCall("http://127.0.0.1:80/itemsQueued/list")
-//        makeGetCall("http://127.0.0.1:80/itemsQueued/listByProfile?limit=10")
+        makeGetCall("/itemsQueued/list")
+        makeGetCall( '/get', [:], 'https://postman-echo.com',)
+        //makeGetCall("http://127.0.0.1:80/itemsQueued/listByProfile?limit=10")
     }
 }
