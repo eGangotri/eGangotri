203e6f0fe29ca94739586d522fbc69736df0ea52
egangotri <egangotritrust@gmail.com>
2023-04-21 01:51:34 +0530

    test test2

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,10 +1,4 @@
 set timestamp=%DATE:/=-%_%TIME::=-%
 set timestamp=%timestamp: =%
-
-git status
-git add src/*
-git add latestJarForUse/*
-git add test/*
-git commit -m "Optimizations @ %timestamp%"
-git push origin master
-git status
+set commit_msg='Optimizations @ %timestamp% %1'
+echo %commit_msg%
