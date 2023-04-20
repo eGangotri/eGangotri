2b933bc7dbb482f90abfe130726c7949d00497fb
egangotri <egangotritrust@gmail.com>
2023-04-21 02:11:37 +0530

    Optimizations at 21-04-2023_2-11-37.04 ''push allows comments''

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,4 +1,13 @@
 set timestamp=%DATE:/=-%_%TIME::=-%
 set timestamp=%timestamp: =%
-set commit_msg='Optimizations @ %timestamp% %1'
-echo %commit_msg%
+set arg1=%1
+set arg1WithoutQuotes=%arg1:"=''%
+set commit_msg="Optimizations at %timestamp% %arg1WithoutQuotes%"
+git status
+git add src/*
+git add latestJarForUse/*
+git add test/*
+git commit -m %commit_msg%
+git push origin master
+git status
+
