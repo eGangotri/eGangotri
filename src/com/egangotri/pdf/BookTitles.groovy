91decd235b22210f124e739eebc07ef33770e107
egangotri <egangotritrust@gmail.com>
2023-04-21 01:37:26 +0530

    Optimizations @ 21-04-2023_1-37-26.66

--- src/com/egangotri/pdf/BookTitles.groovy
+++ src/com/egangotri/pdf/BookTitles.groovy
@@ -1,7 +1,6 @@
 package com.egangotri.pdf
 
 import com.egangotri.util.FileSizeUtil
-import com.egangotri.util.FileUtil
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfReader
 import groovy.util.logging.Slf4j
@@ -17,6 +16,7 @@ import java.text.SimpleDateFormat
 @Slf4j
 class BookTitles {
 
+    static boolean generateCSVAlso = true
     static List<String> FOLDER_NAME = ["C:\\tmp\\pdfForMergeTest\\"]
     static String afterDate = "" //format DD-MM-YYYY
     static int afterHour = 0 //format DD-MM-YYYY
@@ -33,6 +33,7 @@ class BookTitles {
     static String PDF = "pdf"
 
     static StringBuilder MEGA_REPORT = new StringBuilder("")
+    static StringBuilder CSV_MEGA_REPORT = new StringBuilder("")
 
     static boolean DONT_MENTION_SUB_FOLDERS = false;
     static boolean INCLUDE_NUMBER_OF_PAGES = true
@@ -77,7 +78,7 @@ class BookTitles {
         writeToFile()
     }
 
-    static void calculateAfterDate(){
+    static void calculateAfterDate() {
         if (afterDate) {
             if (afterDate.toLowerCase().startsWith("today")) {
                 Calendar date = new GregorianCalendar();
@@ -102,25 +103,55 @@ class BookTitles {
         }
 
     }
-    static void writeToFile() {
-        String _folderNames = (FOLDER_NAME.collect { return new File(it) })*.name.join("_")
-        String fileName = _folderNames + "_MegaList_" +(ONLY_PDFS? "pdfs_only" : "all")
-        File writeableFile = new File(System.getProperty("user.home"), "${fileName}_${new Date().time}.txt")
-        writeableFile << MEGA_REPORT
-        log.info("written to file: ${writeableFile.getAbsolutePath()} ")
+
+    static String generateCsvHeader() {
+        if (generateCSVAlso) {
+            String pageCountHeader = "${INCLUDE_NUMBER_OF_PAGES ? ',Number of Pages' : ''}"
+            String fileSizeHeader = "${INCLUDE_FILE_SIZE ? ",File Size, Units" : ''}"
+            String csvReport = "${INCLUDE_INDEX ? "Serial No., " : ''}File Name${pageCountHeader}${fileSizeHeader}\n"
+            return csvReport
+        }
+        return ""
     }
 
-    static void addToReportAndPrint(String _report, onlyLogDontCommitToFile = false) {
+    static void writeToFile() {
+        String _time = new SimpleDateFormat("dd-MMM-yy-HH-MM-ss").format(new Date().time)
+
+        String _folderNames = (FOLDER_NAME.collect { return new File(it) })*.name.join("_")
+        String fileName = _folderNames + "_MegaList_" + (ONLY_PDFS ? "pdfs_only" : "all")
+        File writeableFile = new File(System.getProperty("user.home"), "${fileName}_${_time}.txt")
+        writeableFile << MEGA_REPORT
+        log.info("written Logs to file: ${writeableFile.getAbsolutePath()} ")
+        if (generateCSVAlso) {
+            File writeableCSVFile = new File(System.getProperty("user.home"), "${fileName}_${_time}.csv")
+            writeableCSVFile << generateCsvHeader()
+            writeableCSVFile << CSV_MEGA_REPORT
+            log.info("written CSV to file: ${writeableCSVFile.getAbsolutePath()} ")
+        }
+    }
+
+    static void addToReportAndPrint(String _report, boolean onlyLogDontCommitToFile = false) {
         log.info(_report)
         if (!onlyLogDontCommitToFile) {
             MEGA_REPORT.append("$_report\n")
         };
     }
 
-    static String formatInteger(Integer _formattable) {
-        def pattern = "##,##,##,###"
-        def moneyform = new DecimalFormat(pattern)
-        return moneyform.format(_formattable.toLong())
+    static void addToCSVReport(String _report) {
+        if (generateCSVAlso) {
+            CSV_MEGA_REPORT.append("$_report\n")
+        };
+    }
+
+    static String formatInteger(Integer _formattable, String delimiter = "") {
+        if(delimiter.contains(",")){
+            return _formattable
+        }
+       else{
+            def pattern = "##,##,##,###"
+            def moneyform = new DecimalFormat(pattern)
+            return moneyform.format(_formattable.toLong())
+        }
     }
 
     static void processOneFolder(String folderAbsolutePath) {
@@ -186,35 +217,42 @@ class BookTitles {
 
     static void printFileNamePageCountFileSize(String folderAbsolutePath, File file, int index) {
         int numberOfPages = 0
-        try{
+        try {
 
-        if (INCLUDE_NUMBER_OF_PAGES && file.name.endsWithIgnoreCase(PDF)) {
-               PdfReader pdfReader = new PdfReader(folderAbsolutePath + "\\" + file.name)
-               PdfDocument pdfDoc = new PdfDocument(pdfReader);
-               numberOfPages = pdfDoc.getNumberOfPages()
-               incrementTotalPageCount(numberOfPages)
-               pdfDoc.close()
-               pdfReader.close()
-        }
+            if (INCLUDE_NUMBER_OF_PAGES && file.name.endsWithIgnoreCase(PDF)) {
+                PdfReader pdfReader = new PdfReader(folderAbsolutePath + "\\" + file.name)
+                PdfDocument pdfDoc = new PdfDocument(pdfReader);
+                numberOfPages = pdfDoc.getNumberOfPages()
+                incrementTotalPageCount(numberOfPages)
+                pdfDoc.close()
+                pdfReader.close()
+            }
 
-        String sizeInfo = FileSizeUtil.getFileSizeFormatted(file)
-        String pageCountLogic = "${INCLUDE_NUMBER_OF_PAGES && file.name.endsWithIgnoreCase(PDF) ? ', ' + numberOfPages + ' Pages' : ''}"
-        String fileSizeLogic = "${INCLUDE_FILE_SIZE && file.name.endsWithIgnoreCase(PDF) ? ', ' + sizeInfo : ''}"
-        String _report = "${INCLUDE_INDEX ? index + ').' : ''} ${file.name} ${pageCountLogic} ${fileSizeLogic}";
-        addToReportAndPrint(_report)
-        incrementFileCount()
+            String sizeInfo = FileSizeUtil.getFileSizeFormatted(file);
+            String pageCountLogic = "${INCLUDE_NUMBER_OF_PAGES && file.name.endsWithIgnoreCase(PDF) ? ', ' + numberOfPages + ' Pages' : ''}";
+            String fileSizeLogic = "${INCLUDE_FILE_SIZE && file.name.endsWithIgnoreCase(PDF) ? ', ' + sizeInfo : ''}";
+            String _report = "${INCLUDE_INDEX ? index + ').' : ''} ${file.name} ${pageCountLogic} ${fileSizeLogic}";
+            addToReportAndPrint(_report);
+            String csvSizeInfo = FileSizeUtil.getFileSizeFormatted(file, ", ")
+            String csvPageCountLogic = "${INCLUDE_NUMBER_OF_PAGES && file.name.endsWithIgnoreCase(PDF) ? numberOfPages : ''}"
+            String csvFileSizeLogic = "${INCLUDE_FILE_SIZE && file.name.endsWithIgnoreCase(PDF) ? csvSizeInfo : ''}"
+            String csvReport = "${INCLUDE_INDEX ? index + ", " : ''}${file.name}, ${csvPageCountLogic}, ${csvFileSizeLogic}"
+            addToCSVReport(csvReport)
+            incrementFileCount()
         }
-        catch(com.itextpdf.kernel.exceptions.BadPasswordException bpe) {
+        catch (com.itextpdf.kernel.exceptions.BadPasswordException bpe) {
             addToErrors(file.absolutePath, true)
             String _report = "${INCLUDE_INDEX ? index + ').' : ''} *****${file.name} is password-protected";
             addToReportAndPrint(_report)
-            log.error("Error in reading Password Protected File for ${file.absolutePath}",bpe)
+            addToCSVReport(_report.split(/\s/, 2).join(", "))
+            log.error("Error in reading Password Protected File for ${file.absolutePath}", bpe)
         }
-        catch(Exception e){
+        catch (Exception e) {
             addToErrors(file.absolutePath)
             String _report = "${INCLUDE_INDEX ? index + ').' : ''} *****${file.name} had an error reading page/count/size";
             addToReportAndPrint(_report)
-            log.error("Error in reading file page-count/size/ for ${file.absolutePath}",e)
+            addToCSVReport(_report.split(/\s/, 2).join(", "))
+            log.error("Error in reading file page-count/size/ for ${file.absolutePath}", e)
         }
     }
 
@@ -222,15 +260,15 @@ class BookTitles {
         TOTAL_FILES_SUCCESSFULLY_READ++
     }
 
-    static void addToErrors(String filePath, boolean passwordProtectedError) {
+    static void addToErrors(String filePath, boolean passwordProtectedError = false) {
         TOTAL_ERRORS++
-        if(!passwordProtectedError){
+        if (!passwordProtectedError) {
             ERRORENOUS_FILES << filePath
-        }
-        else {
+        } else {
             PASSWORD_PROTECTED_FILES << filePath
         }
     }
+
     static void incrementTotalPageCount(int numPagesToIncrement) {
         TOTAL_NUM_PAGES += numPagesToIncrement
     }
@@ -245,17 +283,22 @@ class BookTitles {
         return pdfCount
     }
 
-    static void printFinalStats(){
+    static String generateStats(String delimiter = " ") {
         String totalStats = """
-Total File Count: ${TOTAL_FILES} (** if you are date-filtering then this feature for count of date-filtered not implemented yet)
-Total Files Processed: ${formatInteger(TOTAL_FILES_SUCCESSFULLY_READ+TOTAL_ERRORS)}
-Total Files Read Successfully: ${formatInteger(TOTAL_FILES_SUCCESSFULLY_READ)}
-Total Files with Errors(including password-protected): ${formatInteger(TOTAL_ERRORS)}
-Total Files system didnt pick: ${formatInteger(TOTAL_FILES-(TOTAL_FILES_SUCCESSFULLY_READ+TOTAL_ERRORS))}
-Erroneous File List: ${ERRORENOUS_FILES?"\n"+ERRORENOUS_FILES.join("\t\t\n"):0}
-Password Protected Erroneous File List: ${PASSWORD_PROTECTED_FILES?"\n"+PASSWORD_PROTECTED_FILES.join("\t\t\n"):0}
-Total Pages: ${formatInteger(TOTAL_NUM_PAGES)}"""
-        addToReportAndPrint(totalStats)
+Total File Count:${delimiter}${TOTAL_FILES} (** if you are date-filtering then this feature for count of date-filtered not implemented yet)
+Total Files Processed:${delimiter}${formatInteger(TOTAL_FILES_SUCCESSFULLY_READ + TOTAL_ERRORS,delimiter)}
+Total Files Read Successfully:${delimiter}${formatInteger(TOTAL_FILES_SUCCESSFULLY_READ,delimiter)}
+Total Files with Errors(including password-protected):${delimiter}${formatInteger(TOTAL_ERRORS,delimiter)}
+Total Files system didnt pick:${delimiter}${formatInteger((TOTAL_FILES - (TOTAL_FILES_SUCCESSFULLY_READ + TOTAL_ERRORS)),delimiter)}
+Erroneous File List:${delimiter}${ERRORENOUS_FILES ? "\n" + ERRORENOUS_FILES.join("\t\t\n") : 0}
+Password Protected Erroneous File List:${delimiter}${PASSWORD_PROTECTED_FILES ? "\n" + PASSWORD_PROTECTED_FILES.join("\t\t\n") : 0}
+Total Pages:${delimiter}${formatInteger(TOTAL_NUM_PAGES,delimiter)}"""
+        return totalStats
+    }
+
+    static void printFinalStats() {
+        addToReportAndPrint(generateStats())
+        addToCSVReport(generateStats(", "))
     }
 
 
--- src/com/egangotri/util/FileSizeUtil.groovy
+++ src/com/egangotri/util/FileSizeUtil.groovy
@@ -4,29 +4,29 @@ import groovy.io.FileType
 
 class FileSizeUtil {
 
-    static String formatFileSize(BigDecimal sizeInKB){
+    static String formatFileSize(BigDecimal sizeInKB, String delimiter = " "){
         BigDecimal sizeInMB = sizeInKB/1024
         BigDecimal sizeInGB = sizeInMB/1024
-        return sizeInKB >= 1024 ? (sizeInMB >= 1024 ? "${sizeInGB.round(2)} GB" : "${sizeInMB.round(2)} MB") : "${sizeInKB.round(2)} KB"
+        return sizeInKB >= 1024 ? (sizeInMB >= 1024 ? "${sizeInGB.round(2)}${delimiter}GB" : "${sizeInMB.round(2)}${delimiter}MB") : "${sizeInKB.round(2)}${delimiter}KB"
     }
 
     static BigDecimal fileSizeInKBForPath(String absPath) {
         return fileSizeInKB(new File(absPath))
     }
 
-    static BigDecimal fileSizeInKB(File file) {
+    static BigDecimal fileSizeInKB(File file, String delimiter = " ") {
         if(file && file.exists()){
             return ((file?.size()/ 1024) as BigDecimal)
         }
         return "Empty or Non-Existent File"
     }
 
-    static String getFileSizeFormatted(String absPath){
-        return formatFileSize(fileSizeInKBForPath(absPath))
+    static String getFileSizeFormatted(String absPath, String delimiter = " "){
+        return formatFileSize(fileSizeInKBForPath(absPath),delimiter)
     }
 
-    static String getFileSizeFormatted(File file){
-        return formatFileSize(fileSizeInKB(file))
+    static String getFileSizeFormatted(File file, String delimiter = " "){
+        return formatFileSize(fileSizeInKB(file),delimiter)
     }
 
     /*
