2690baabcb0340bb9b3614f44a388a232f9128b8
eGangotri <egangotritrust@gmail.com>
2023-08-10 08:44:21 +0530

    .

--- src/com/egangotri/pdf/BookTitles.groovy
+++ src/com/egangotri/pdf/BookTitles.groovy
@@ -133,7 +133,7 @@ class BookTitles {
         String _time = new SimpleDateFormat("dd-MMM-yy-HH-MM-ss").format(new Date().time)
 
         String _folderNames = (FOLDER_NAMES.collect { return new File(it) })*.name.join("_")
-        String fileName = _folderNames + "_MegaList_" + (ONLY_PDFS ? "pdfs_only" : "all") + "_${TOTAL_FILES_SUCCESSFULLY_READ}"
+        String fileName = _folderNames + "_MegaList_" + (ONLY_PDFS ? "pdfs_only" : "all")
         String directoryName = DUMP_DIRECTORY + "//${_folderNames}"
         Files.createDirectories(Paths.get(directoryName));
         String _fileName = "${fileName}_${_time}_${TOTAL_FILES_SUCCESSFULLY_READ}"
