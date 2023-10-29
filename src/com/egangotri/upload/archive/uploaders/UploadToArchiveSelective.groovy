91d13b04207df3153e0f57754baeda4e1b228950
egangotri <egangotritrust@gmail.com>
2023-10-29 09:41:16 +0800

    -

--- src/com/egangotri/upload/archive/uploaders/UploadToArchiveSelective.groovy
+++ src/com/egangotri/upload/archive/uploaders/UploadToArchiveSelective.groovy
@@ -1,22 +1,54 @@
 package com.egangotri.upload.archive.uploaders
 
+import com.egangotri.upload.archive.ArchiveHandler
 import com.egangotri.upload.archive.UploadToArchive
+import com.egangotri.upload.util.ArchiveUtil
+import com.egangotri.upload.util.FileRetrieverUtil
 import com.egangotri.upload.util.SettingsUtil
 import com.egangotri.upload.util.UploadUtils
+import com.egangotri.upload.vo.QueuedVO
 import com.egangotri.util.EGangotriUtil
 import groovy.util.logging.Slf4j
 
 @Slf4j
 class UploadToArchiveSelective {
     static void main(String[] args) {
-        List<String> archiveProfiles = []
+        String archiveProfile = ""
+        String fileName = ""
         if (args && args.length == 2) {
             log.info "args $args"
-            archiveProfiles = args[0]
+            archiveProfile = args[0]
+            fileName = args[1].endsWith(EGangotriUtil.PDF) ? args[1] : args[1] + EGangotriUtil.PDF;
         } else {
-            log.info "Must have 2 arg.s Profile name and title of pdf"
+            log.info "Must have 2 arg.s Profile name and fileName of pdf"
             System.exit(0)
         }
         UploadToArchive.prelims(args)
+        String localPath = getLocalPath(archiveProfile, fileName)
+        Set<QueuedVO> vos = ArchiveUtil.generateVOsFromFileNames(args[0], [localPath])
+        log.info("localPath ${localPath}")
+        log.info("vos ${vos}")
+        List<Integer> uploadStats = ArchiveHandler.uploadAllItemsToArchiveByProfile(UploadToArchive.metaDataMap, vos as Set<QueuedVO>)
+    }
+
+    static String getLocalPath(String archiveProfile, String fileName) {
+        String _folder = FileRetrieverUtil.pickFolderBasedOnArchiveProfile(archiveProfile)
+        String filePath = ""
+        File folder = new File(_folder)
+        if (folder.exists() && folder.isDirectory()) {
+            // Create a File object for the file inside the folder
+            def file = new File(folder, fileName)
+            // Check if the file exists
+            if (file.exists() && file.isFile()) {
+                // Get the absolute path of the file
+                filePath = file.getCanonicalPath()
+                println "Absolute path of $fileName: $filePath"
+            } else {
+                println "File $fileName does not exist in the folder."
+            }
+        } else {
+            println "Folder $folder does not exist."
+        }
+        return filePath;
     }
 }
