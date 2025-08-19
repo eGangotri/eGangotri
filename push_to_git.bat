57fc09300e0c4e961b23b07bd992cad7e4ce77ec
writetochetanpandey <writetochetanpandey@gmail.com>
2025-08-19 16:36:39 +0530

    Optimizations at Tue08-19-2025_16-36-39.23 ='

--- push_to_git.bat
+++ push_to_git.bat
@@ -5,6 +5,8 @@ set arg1WithoutQuotes=%arg1:"='%
 set commit_msg="Optimizations at %timestamp% %arg1WithoutQuotes%"
 git status
 git add src/*
+git add .gitignore
+git add push_to_git.bat 
 git add latestJarForUse/*
 git add test/*
 git commit -m %commit_msg%
