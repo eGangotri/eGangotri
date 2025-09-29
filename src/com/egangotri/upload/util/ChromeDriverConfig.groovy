26cfa386e4f30a69805ca8000db412d46693c6f5
writetochetanpandey <writetochetanpandey@gmail.com>
2025-09-29 20:59:14 +0530

    Optimizations at Mon09-29-2025_20-59-14.46 ='

--- src/com/egangotri/upload/util/ChromeDriverConfig.groovy
+++ src/com/egangotri/upload/util/ChromeDriverConfig.groovy
@@ -2,6 +2,7 @@ package com.egangotri.upload.util
 
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.PageLoadStrategy
 import java.time.Duration
 
 class ChromeDriverConfig {
@@ -16,8 +17,15 @@ class ChromeDriverConfig {
             '--no-sandbox',
             '--disable-gpu',  // Helps with headless mode stability
             '--disable-software-rasterizer',  // Additional stability option
-            '--disable-extensions'  // Disables extensions that might interfere
+            '--disable-extensions',  // Disables extensions that might interfere
+            // Force direct connection (bypass system/corp proxies) to avoid ERR_CONNECTION_TIMED_OUT
+            '--proxy-server=direct://',
+            '--proxy-bypass-list=*'
         ])
+        // Prefer faster ready state to avoid long hangs on slow resources
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER)
+        // Be lenient with certs (should not be needed for archive.org, but avoids sporadic failures)
+        options.setAcceptInsecureCerts(true)
         
         // Create driver with options
         ChromeDriver driver = new ChromeDriver(options)
