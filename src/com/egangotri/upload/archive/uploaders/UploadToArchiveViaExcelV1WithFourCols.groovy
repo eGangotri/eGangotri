6bfc20013f88b5b8c9e6e1b9efff954f8ab4c2c2
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2026-08-14 21:40:30 +0530

    Optimizations at Fri08-14-2026_21-40-29.99 ='

--- src/com/egangotri/upload/archive/uploaders/UploadToArchiveViaExcelV1WithFourCols.groovy
+++ src/com/egangotri/upload/archive/uploaders/UploadToArchiveViaExcelV1WithFourCols.groovy
@@ -80,7 +80,7 @@ class UploadToArchiveViaExcelV1WithFourCols {
         List<UploadItemFromExcelVO> uploadItems = excelData.uploadItems
         log.info("uploadItems(${uploadItems.size()}) ${uploadItems[0]?.subject} ${uploadItems[0]?.description} ${uploadItems[0]?.creator} ${uploadItems[0]?.absolutePath}")
         Map<Integer, String> uploadSuccessCheckingMatrix = [:]
-        UploadersUtil.addToUploadCycleWithModeV2(archiveProfile, uploadItems*.absolutePath, "Excel-;${excelFileName}-;${range}")
+        UploadersUtil.addToUploadCycleWithModeV2(archiveProfile, uploadItems*.absolutePath, "Excel-v1-4Col-${range}")
 
         Set<QueuedVO> vos = ArchiveUtil.generateVOsFromSuppliedData(archiveProfile, uploadItems)
         if (uploadItems) {
